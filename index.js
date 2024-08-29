4ae50c6b142be65c7d0c6e21ec1f1dd8e62a168f
Eduardo Vergara <eduvergara.dev@gmail.com>
2024-08-29 14:24:36 +1000

    part3 - updates on phone number regex validation

--- index.js
+++ index.js
@@ -1,4 +1,3 @@
-// library loading
 require('dotenv').config()
 const express = require('express')
 const morgan = require('morgan')
--- models/person.js
+++ models/person.js
@@ -23,7 +23,7 @@ const personSchema = new mongoose.Schema({
     type: String,
     validate: {
       validator: function (v) {
-        return /\d{3}-\d{7}$/.test(v)
+        return /\d{10}$/.test(v)
       },
       message: (props) => `${props.value} is not a valid phone number!`,
     },
